24762bb9c3d700fab663033cc5e7318dfc2189c6
Nick <boodoo@temple.edu>
2024-03-14 18:20:56 -0400

    added screen package

--- code/main.py
+++ code/main.py
@@ -1,11 +1,19 @@
+from game.world_states import WorldStates
 from game.game_loop import GameLoop
 from game.player import Player
-from game.world_states import WorldStates
+
 
 
 if __name__ == "__main__":
+
     world_state = WorldStates()
+
+
     player_name = input("Enter your name: ")
     player = Player(player_name)
+
+
     game_loop = GameLoop(player, world_state)
+
+
     game_loop.start_loop()
