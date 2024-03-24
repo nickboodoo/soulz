e04d61035e123402a38447af324b8c0d00af91af
Nick <boodoo@temple.edu>
2024-03-24 07:09:34 -0400

    simplified code further

--- game/soulz.py
+++ game/soulz.py
@@ -1,13 +1,9 @@
 from code.game_setup import GameSetup
-from code.character import Player
 
 
 if __name__ == "__main__":
 
     node_difficulties = [("A", 0.7), ("B", 0.4), ("C", 0.9), ("D", 0.5), ("E", 0.8), ("F", 0.6)]
-
-    
-    player = Player()
 
     setup = GameSetup(node_difficulties, "A", "D")
     setup.setup_game()
