a959271bc8585127fd0d49c3bf424e0b8f6259a2
Nick <boodoo@temple.edu>
2024-03-24 05:44:57 -0400

    fixed errors

--- game/soulz.py
+++ game/soulz.py
@@ -1,11 +1,9 @@
-from code.dynamic_world_map import DynamicWorldMap
-from code.gameplay_manager import GameplayManager
+from code.game_setup import GameSetup
 
 
 if __name__ == "__main__":
     node_difficulties = [("A", 0.7), ("B", 0.4), ("C", 0.9), ("D", 0.5), ("E", 0.8), ("F", 0.6)]
-    graph = DynamicWorldMap()
-    graph.generate_graph(node_difficulties)
+    setup = GameSetup(node_difficulties, "A", "D")
+    setup.setup_game()
+    setup.initiate_gameplay_loop()
 
-    game = GameplayManager(graph, "A", "D")
-    game.initiate_gameplay_loop()
